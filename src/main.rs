217dc1c646ed8c2977147a3c871cc54c5b75c532
Colleen <cdnickerson05@gmail.com>
2023-11-09 20:22:12 -0400

    x

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,68 @@
 use raylib::prelude::*;
 
-fn main() {
-    let (mut rl, thread) = raylib::init().size(640, 480).title("Hello, World").build();
+struct GameTextures {
+    menu_background: Texture2D,
+    field_cells: Texture2D,
+    top_cells: Texture2D,
+    status_bar: Texture2D,
+}
+struct GameSounds {
+    explode: Sound,
+    win: Sound,
+}
 
-    while !rl.window_should_close() {
-        let mut d = rl.begin_drawing(&thread);
+struct GameAssets {
+    icon: Image,
+    textures: GameTextures,
+}
 
+struct Game {
+    rl: RaylibHandle,
+    thread: RaylibThread,
+    assets: GameAssets,
+}
+
+impl Game {
+    fn draw(&mut self) {
+        let mut d = self.rl.begin_drawing(&self.thread);
         d.clear_background(Color::WHITE);
-        d.draw_text("Hello, world!", 12, 12, 20, Color::BLACK);
+        d.draw_texture(&self.assets.textures.menu_background, 0, 0, Color::WHITE)
     }
+
+    fn update(&mut self) {}
+
+    fn start(&mut self) {
+        self.rl.set_window_icon(&self.assets.icon);
+
+        while !self.rl.window_should_close() {
+            self.update();
+            self.draw();
+        }
+    }
+
+    fn new(title: &str, width: i32, height: i32) -> Game {
+        let (mut rl, thread) = raylib::init().size(width, height).title(title).build();
+        let assets = GameAssets {
+            icon: Image::load_image("./resources/icon.png").expect("Could not load image."),
+            textures: GameTextures {
+                menu_background: rl
+                    .load_texture(&thread, "./resources/textures/splash.png")
+                    .expect("Could not load texture."),
+                field_cells: rl
+                    .load_texture(&thread, "./resources/textures/fieldcells.png")
+                    .expect("Could not load texture."),
+                top_cells: rl
+                    .load_texture(&thread, "./resources/textures/topcells.png")
+                    .expect("Could not load texture."),
+                status_bar: rl
+                    .load_texture(&thread, "./resources/textures/statusbar.png")
+                    .expect("Could not load texture."),
+            },
+        };
+        Game { rl, thread, assets }
+    }
+}
+
+fn main() {
+    Game::new("Mines", 800, 600).start();
 }
